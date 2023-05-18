e52a5ad5b15d4f8a9b3e9587d8ebd0d757a1a203
Bolzenpinguin <68389916+Bolzenpinguin@users.noreply.github.com>
2023-05-18 12:23:13 +0200

    Create basic Menu

--- java_projekt/Tipp1/src/Main.java
+++ java_projekt/Tipp1/src/Main.java
@@ -1,14 +1,14 @@
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            MainFrame frame = new MainFrame();
-            frame.setTitle("Tipp1");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setLocationRelativeTo(null);
-            frame.setSize(500, 500);
-            frame.setVisible(true);
-        });
-    }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			MainFrame frame = new MainFrame();
+			frame.setTitle("Tipp1");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLocationRelativeTo(null);
+			frame.setSize(500, 500);
+			frame.setVisible(true);
+		});
+	}
 }
