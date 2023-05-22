dd8e4f189d659ab74ff221b5bf5e7ac3eb342c93
IchbinN4b <brandtsebastian02@gmail.com>
2023-05-22 21:43:58 +0200

    javadoc main

--- java_projekt/Tipp1/src/Main.java
+++ java_projekt/Tipp1/src/Main.java
@@ -1,15 +1,38 @@
 import javax.swing.*;
 
+/**
+ * The "Main" class contains the main method to start the application.
+ * It creates an instance of the "MainFrame" class and sets up the main window.
+ */
 public class Main {
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			MainFrame frame = new MainFrame();
-			frame.setTitle("Tipp1");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.pack();
-			frame.setLocationRelativeTo(null);
-			frame.setSize(1500, 800);
-			frame.setVisible(true);
-		});
-	}
+    /**
+     * The main method of the application.
+     * It creates an instance of "MainFrame", configures the main window, and makes it visible.
+     *
+     * @param args The command-line arguments (not used in this application).
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // Create an instance of the "MainFrame" class
+            MainFrame frame = new MainFrame();
+
+            // Set the title of the main window
+            frame.setTitle("Tipp1");
+
+            // Set the default close operation of the main window
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // Pack the components in the main window
+            frame.pack();
+
+            // Center the main window on the screen
+            frame.setLocationRelativeTo(null);
+
+            // Set the size of the main window
+            frame.setSize(1000, 500);
+
+            // Make the main window visible
+            frame.setVisible(true);
+        });
+    }
 }
