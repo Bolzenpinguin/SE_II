f1b20134306019bfb0fac47b45943672d6486982
IchbinN4b <brandtsebastian02@gmail.com>
2023-05-22 21:41:51 +0200

    Javadoc englisch Teil 2

--- java_projekt/Tipp1/src/Help.java
+++ java_projekt/Tipp1/src/Help.java
@@ -2,26 +2,40 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 
+/**
+ * The "Help" class extends JPanel and represents a help view.
+ * It contains a text area for displaying help information and a back button
+ * to return to the main menu.
+ */
 public class Help extends JPanel {
+
+    /**
+     * Constructor for the "Help" class.
+     *
+     * @param frame The main window (MainFrame) that contains the help view.
+     */
     public Help(MainFrame frame) {
         setLayout(new BorderLayout());
 
+        // Create the text area for help information
         JTextArea helpText = new JTextArea();
         helpText.setEditable(false);
 
+        // Create a scroll pane to make the text area scrollable
         JScrollPane scrollPane = new JScrollPane(helpText);
         add(scrollPane, BorderLayout.CENTER);
 
-        JButton backButton = new JButton("Zurück zum Hauptmenü");
+        // Create the back button
+        JButton backButton = new JButton("Back to Main Menu");
         backButton.addActionListener(e -> frame.showPanel("MainMenu"));
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        buttonPanel.add(backButton);
-        add(buttonPanel, BorderLayout.PAGE_END);
+        add(backButton, BorderLayout.PAGE_END);
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("java_projekt/Tipp1/textContent/Help.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("textContent/Help.txt"))) {
+            // Read the help information from the text file and display it in the text area
             helpText.read(reader, null);
         } catch (IOException e) {
+            // If the file is not found or cannot be read, display an error message
             helpText.setText("Help file not found!");
         }
     }
--- java_projekt/Tipp1/src/MainFrame.java
+++ java_projekt/Tipp1/src/MainFrame.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The "MainFrame" class extends JFrame and represents the main window of the application.
+ * It manages the different panels/views using CardLayout, including the main menu, help,
+ * play, scoreboard, and instructions.
+ */
 public class MainFrame extends JFrame {
     MainMenu mainMenu;
     Play play;
@@ -9,16 +14,22 @@ public class MainFrame extends JFrame {
     Instructions instructions;
     CardLayout cardLayout;
 
+    /**
+     * Constructor for the "MainFrame" class.
+     * Initializes the CardLayout and creates instances of the different panels/views.
+     */
     public MainFrame() {
         cardLayout = new CardLayout();
         setLayout(cardLayout);
 
+        // Create instances of the different panels/views
         mainMenu = new MainMenu(this);
         help = new Help(this);
+        play = new Play(this,scoreBoard);
         scoreBoard = new ScoreBoard(this);
-        play = new Play(this, scoreBoard);
         instructions = new Instructions(this);
 
+        // Add the panels/views to the main frame with corresponding names
         add(mainMenu, "MainMenu");
         add(help, "Help");
         add(play, "Play");
@@ -26,6 +37,11 @@ public class MainFrame extends JFrame {
         add(instructions, "Instructions");
     }
 
+    /**
+     * Switches the currently displayed panel/view based on the provided name.
+     *
+     * @param name The name of the panel/view to show.
+     */
     public void showPanel(String name) {
         cardLayout.show(getContentPane(), name);
     }
